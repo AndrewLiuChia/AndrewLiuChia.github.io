81c6c99f630e203639727d53df56f8f61651e7f1
DESKTOP-QQBRP66\Andrew <m9721013@gmail.com>
2024-07-04 23:23:06 +0800

    修改159169portchange.py程式

--- file/159169portchange.py
+++ file/159169portchange.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 # 定義讀取檔案的函數
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -12,11 +14,12 @@ def write_file(file_path, lines):
         file.writelines(lines)
 
 # 定義更改 port 的函數
-def change_port(lines, old_ip_port, new_ip_port):
+def change_port(lines, ip_address, new_port):
     new_lines = []
     for line in lines:
-        if old_ip_port in line:
-            new_line = line.replace(old_ip_port, new_ip_port)
+        pattern = re.compile(f'{ip_address}:\d+')
+        if pattern.search(line):
+            new_line = pattern.sub(f'{ip_address}:{new_port}', line)
             new_lines.append(new_line)
         else:
             new_lines.append(line)
@@ -24,24 +27,28 @@ def change_port(lines, old_ip_port, new_ip_port):
 
 # 主函數
 def main():
-    input_file_path = r'C:\Users\Andrew\Source\Repos\GithubPage\file\combine.txt'  # 原始檔案路徑
+    file_paths = [
+        r'C:\Users\Andrew\Source\Repos\GithubPage\file\combine.txt',
+        r'C:\Users\Andrew\Source\Repos\GithubPage\file\TaiwanLive.txt'
+    ]  # 原始檔案路徑
 
-    # 定義需要替換的 IP 和端口
+    # 定義需要替換的 IP 和新 port
     replacements = {
-        '108.181.20.159:55137': '108.181.20.159:12345',
-        '108.181.32.169:34159': '108.181.32.169:54321'
+        '108.181.20.159': '55137',
+        '108.181.32.169': '34159'
     }
 
-    # 讀取檔案內容
-    lines = read_file(input_file_path)
+    for file_path in file_paths:
+        # 讀取檔案內容
+        lines = read_file(file_path)
 
-    # 更改指定的 IP 和端口
-    for old_ip_port, new_ip_port in replacements.items():
-        lines = change_port(lines, old_ip_port, new_ip_port)
+        # 更改指定的 IP 和 port
+        for ip_address, new_port in replacements.items():
+            lines = change_port(lines, ip_address, new_port)
 
-    # 將修改後的內容寫入原檔案
-    write_file(input_file_path, lines)
-    print(f"已將新的內容寫入 {input_file_path}")
+        # 將修改後的內容寫入原檔案
+        write_file(file_path, lines)
+        print(f"已將新的內容寫入 {file_path}")
 
 # 執行主函數
 if __name__ == "__main__":
